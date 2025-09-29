06e097e167cb19c47b7f3e17ef27eea12e3c68ad
Edmh <edavidmanotas@gmail.com>
2025-09-29 00:53:41 -0500

    feat: configuracion de rutas

---
+++ src/main/java/com/zapaticorp/gateway/config/GatewayConfig.java
@@ -0,0 +1,49 @@
+package com.zapaticorp.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Value("${URI_AUTH}")
+    private String authUri;
+
+    @Value("${URI_VIAJERO}")
+    private String viajeroUri;
+
+    @Value("${URI_ACTIVIDAD}")
+    private String actividadUri;
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+                // Ruta al microservicio de autenticación
+                .route("auth-service", r -> r.path("/auth/**")
+                        .filters(f -> f.addRequestHeader("X-Gateway", "Gateway-auth"))
+                        .uri(authUri))
+
+                // Ruta al microservicio de viajeros
+                .route("viajero-service", r -> r.path("/api/viajeros/**")
+                        .filters(f -> f.addRequestHeader("X-Gateway", "Gateway-Viajero"))
+                        .uri(viajeroUri))
+
+                // Ruta al microservicio de actividades
+                .route("actividad-service", r -> r
+                        .path("/api/actividades/**")
+                        .or()
+                        .path("/api/ubicaciones/**")
+                        .or()
+                        .path("/api/tipoActividades/**")
+                        .or()
+                        .path("/api/viajes/**")
+                        .filters(f -> f.addRequestHeader("X-Gateway", "Gateway-Actividades"))
+                        .uri(actividadUri))
+
+                .build();
+    }
+}
