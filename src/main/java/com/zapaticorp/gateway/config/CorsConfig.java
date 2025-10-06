fc6f941b2893baf25f527e672de7aad9a3f1b77b
Edmh <edavidmanotas@gmail.com>
2025-10-06 08:39:17 -0500

    chore: Cors update

--- src/main/java/com/zapaticorp/gateway/config/CorsConfig.java
+++ src/main/java/com/zapaticorp/gateway/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
